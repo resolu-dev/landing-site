2d1a9258b74ba86ad66f7372c04a8e0e19258291
resolu-dev <resolu.info.ph@gmail.com>
2023-08-29 02:53:38 +0800

    Added fix on hamburger function and sidebar design, also fixed navbar and scroll down issue

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
+const hamburger = document.querySelector(".hamburger");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+const overlay = document.querySelector(".overlay");
 
-window.addEventListener("scroll", reveal);
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  sidebar.classList.toggle("active");
+  overlay.style.display = "block";
+
+  if (sidebar.classList.contains("active")) {
+    // Prevent scrolling when sidebar is active
+    document.body.style.overflow = "hidden";
+  } else {
+    // Allow scrolling when sidebar is closed
+    document.body.style.overflow = "auto";
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  sidebar.classList.remove("active");
+  overlay.style.display = "none";
+
+  // Allow scrolling when sidebar is closed
+  document.body.style.overflow = "auto";
+});
