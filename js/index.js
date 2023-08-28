3adb0a829cb8905b5b1e1e85983f1a5ed83f89a0
vlb-dotcom <barrera.vincentlloyd@gmail.com>
2023-08-29 06:03:49 +0800

    Added function when a user clicks the overlay, it will hide the sidebar

--- js/index.js
+++ js/index.js
@@ -1,27 +1,58 @@
+// const hamburger = document.querySelector(".hamburger");
+// const sidebar = document.querySelector(".sidebar");
+// const closeBtn = document.querySelector(".close-btn");
+// const overlay = document.querySelector(".overlay");
+
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   sidebar.classList.toggle("active");
+//   overlay.style.display = "block";
+
+//   if (sidebar.classList.contains("active")) {
+//     // Prevent scrolling when sidebar is active
+//     document.body.style.overflow = "hidden";
+//   } else {
+//     // Allow scrolling when sidebar is closed
+//     document.body.style.overflow = "auto";
+//   }
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   hamburger.classList.remove("active");
+//   sidebar.classList.remove("active");
+//   overlay.style.display = "none";
+
+//   // Allow scrolling when sidebar is closed
+//   document.body.style.overflow = "auto";
+// });
+
 const hamburger = document.querySelector(".hamburger");
 const sidebar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector(".close-btn");
 const overlay = document.querySelector(".overlay");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  sidebar.classList.toggle("active");
-  overlay.style.display = "block";
-
-  if (sidebar.classList.contains("active")) {
-    // Prevent scrolling when sidebar is active
-    document.body.style.overflow = "hidden";
-  } else {
-    // Allow scrolling when sidebar is closed
-    document.body.style.overflow = "auto";
-  }
+  toggleSidebar();
 });
 
 closeBtn.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  sidebar.classList.remove("active");
-  overlay.style.display = "none";
-
-  // Allow scrolling when sidebar is closed
-  document.body.style.overflow = "auto";
+  toggleSidebar();
 });
+
+overlay.addEventListener("click", () => {
+  toggleSidebar();
+});
+
+function toggleSidebar() {
+  hamburger.classList.toggle("active");
+  sidebar.classList.toggle("active");
+  overlay.style.display = sidebar.classList.contains("active")
+    ? "block"
+    : "none";
+
+  if (sidebar.classList.contains("active")) {
+    document.body.style.overflow = "hidden"; // Prevent scrolling when sidebar is active
+  } else {
+    document.body.style.overflow = "auto"; // Allow scrolling when sidebar is closed
+  }
+}
