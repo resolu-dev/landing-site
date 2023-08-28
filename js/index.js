93e7a5dffc596e2c2a803cae5b5f7d11f4e80560
vlb-dotcom <barrera.vincentlloyd@gmail.com>
2023-08-29 06:04:36 +0800

    Cleaned the cod in index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
-// const hamburger = document.querySelector(".hamburger");
-// const sidebar = document.querySelector(".sidebar");
-// const closeBtn = document.querySelector(".close-btn");
-// const overlay = document.querySelector(".overlay");
-
-// hamburger.addEventListener("click", () => {
-//   hamburger.classList.toggle("active");
-//   sidebar.classList.toggle("active");
-//   overlay.style.display = "block";
-
-//   if (sidebar.classList.contains("active")) {
-//     // Prevent scrolling when sidebar is active
-//     document.body.style.overflow = "hidden";
-//   } else {
-//     // Allow scrolling when sidebar is closed
-//     document.body.style.overflow = "auto";
-//   }
-// });
-
-// closeBtn.addEventListener("click", () => {
-//   hamburger.classList.remove("active");
-//   sidebar.classList.remove("active");
-//   overlay.style.display = "none";
-
-//   // Allow scrolling when sidebar is closed
-//   document.body.style.overflow = "auto";
-// });
-
 const hamburger = document.querySelector(".hamburger");
 const sidebar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector(".close-btn");
